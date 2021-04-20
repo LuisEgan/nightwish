bbdb67201f39a5ce32785946a5b2035929dbdd24
L <l@cleveritgroup.com>
2021-04-20 17:04:05 -0400

    contstants

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const ROUTES = {
+  PRIVATE_ROUTES: {
+    lobby: "/lobby",
+  },
+
+  PUBLIC_ROUTES: {
+    home: "/home",
+    index: "/",
+  },
+};
+
+export const LOCAL_STORAGE = {
+  USER_TOKEN: "USER_TOKEN",
+};
